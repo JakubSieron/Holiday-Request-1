77518e7e75558f91dae30a8a1a134627e92f4852
Jakub <thyned@gmail.com>
2015-07-31 08:07:05 +0100

    16:23

--- app/controllers/hrs_controller.rb
+++ app/controllers/hrs_controller.rb
@@ -1,4 +1,49 @@
 class HrsController < ApplicationController
+	before_action :find_hr, only: [:show, :edit, :update, :destroy]
+
 	def index
+	end
+
+    def show
+	end
+
+	def new
+		@hr = Hr.new
+	end
+
+	def create
+		@hr = Hr.new(hr_params)
+
+		if @hr.save
+			redirect_to @hr
+		else
+			render'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @hr.update
+			redirect_to @hr
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@hr.destroy
+		redirec_to root_path
+	end
+
+	private
+
+	def find_hr
+		@hr = Hr.find(params[:id])
+	end
+
+	def hr_params
+		params.require(:hr).permit(:from, :to)
 	end
 end
